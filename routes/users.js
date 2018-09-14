8cdfa4bf0934d8f847169b36858dce8b289244d7
onildo <onildo@norteldata.com.br>
2018-09-14 20:02:21 -0300

    Botão stop para o lado cliente parar de fazer as consultas infinitas

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.post('/', function(req, res, next) {
   let random = Math.random() * (5000 - 1000) + 1000
   setTimeout(function () {res.send(random.toString())}, random)
 });

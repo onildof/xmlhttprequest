29322da607d2b7043ae64eacb6f017ce6ca48daf
onildo <onildo@norteldata.com.br>
2018-09-15 01:16:57 -0300

    Cliente envia ordem para servidor começar operação infinita (gera intervalos de tempo aleatórios que são somados a um acumulador). Cliente consulta os valores do servidor a cada 1000ms. Cliente pode enviar ordem para o servidor parar a operação.

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,46 @@
 var express = require('express');
 var router = express.Router();
+let soma = 0
+let novoNumero = 0
+let running = false
 
 /* GET users listing. */
-router.post('/', function(req, res, next) {
-  let random = Math.random() * (5000 - 1000) + 1000
-  setTimeout(function () {res.send(random.toString())}, random)
+router.post('/iniciar', function(req, res, next) {
+    if (running == false) {
+        running = true
+        longOperation()
+    }
+    
+    function longOperation() {
+            let random = Math.floor(Math.random() * (5000 - 1000) + 1000)
+            
+            setTimeout(function () {
+                if (running == true) {
+                    novoNumero = random
+                    soma += novoNumero
+                    console.log("Soma:", soma)
+                    console.log("Novo:", novoNumero)
+                    longOperation()
+                }
+            }, random)
+    }
+    
+    res.send("INICIADO")
 });
+
+
+router.post('/parar', function(req, res, next) {
+    running = false
+    res.send("PARADO")
+})
+
+router.get('/consultar', function(req, res, next) {
+    let resultado = {
+        soma: soma,
+        novoNumero: novoNumero
+    }
+
+    res.send(JSON.stringify(resultado))
+})
 
 module.exports = router;
